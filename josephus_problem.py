dd2d6379ef6788192528796c561b10f03cf5ea28
paritoshborkar <pb2982@rit.edu>
2018-12-10 20:51:29 -0500

    Josephus problem position

--- josephus_problem.py
+++ josephus_problem.py
@@ -1,5 +1,13 @@
-from queue_implementation import *
+"""
+People are standing in a circle waiting to be executed. 
+Counting begins at a specified point in the circle and proceeds around the circle in a specified direction. 
+After a specified number of people are skipped, the next person is executed. 
+The procedure is repeated with the remaining people, starting with the next person, 
+going in the same direction and skipping the same number of people, until only one person remains, and is freed.
 
+The problem — given the number of people, starting point, direction, and number to be skipped — 
+is to choose the position in the initial circle to avoid execution. (From Wikipedia)
+"""
 
 def getPosition(n, k):
     start = 0
